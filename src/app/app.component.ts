5b5319dcf9bc44f196f2e521b4ca9125b7091ada
Pawan Gautam <pawangtm2419@gmail.com>
2021-08-09 11:59:18 +0530

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickGreen', [
+      state('true', style({
+        backgroundColor: 'green'
+      })),
+      state('false', style({
+        backgroundColor: 'red'
+      })),
+      transition('true => false', animate('2000ms linear')),
+      transition('false => true', animate('2000ms linear'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'animation';
+  isGreen: boolean = true;
+
+  toggleIsCorrect() {
+    this.isGreen = this.isGreen ? false : true;
+  }
 }
